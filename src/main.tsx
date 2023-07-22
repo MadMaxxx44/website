9fa8ffe0dd33fd02010744ba94ce49b485522557
Max <andriiuk.maks@gmail.com>
2023-07-22 21:41:39 +0300

    browser router removed

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,5 @@
-// import React from 'react'
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Menu from "./pages/Menu.tsx";
-// import AboutUs from "./pages/AboutUs.tsx";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
@@ -13,10 +9,5 @@ AOS.init({
 });
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      {/* <Route path="/about-us" element={<AboutUs />} /> */}
-    </Routes>
-  </BrowserRouter>
+  <App />
 );
